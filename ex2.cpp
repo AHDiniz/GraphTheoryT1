fdefa4eb4cf44655342cfccce16885d21b6e508c
Gabriel Ferrari Wagnitz <gabriel.wagnitz@gmail.com>
2019-10-26 13:46:08 -0300

    Using Malloc for adj

--- ex2.cpp
+++ ex2.cpp
@@ -9,102 +9,101 @@
 template<typename T>
 inline void remove(std::vector<T> &v, const T e)
 {
-	v.erase(std::remove(v.begin(), v.end(), e), v.end());
-}
-
-template<typename T>
-inline bool contains(const std::vector<T> &v, const T e)
-{
-	return std::find(v.begin(), v.end(), e) != v.end();
+    v.erase(std::remove(v.begin(), v.end(), e), v.end());
 }
 
 class Graph
 {
 public:
-	Graph(int n, int m);
+    Graph(int n, int m);
+    ~Graph();
 
-	inline void set(const int u, const int v) { adj[u * n + v] = 1; }
-	inline void unset(const int u, const int v) { adj[u * n + v] = 0; }
-	inline int get(const int u, const int v) const { return adj[u * n + v]; }
+    inline void set(const int u, const int v) { adj[u * n + v] = 1; }
+    inline void unset(const int u, const int v) { adj[u * n + v] = 0; }
+    inline int get(const int u, const int v) const { return adj[u * n + v]; }
 
-	const int dijkstra(int s, int e) const;
-
+    const int dijkstra(int s, int e) const;
 private:
-	int n, m;
-	std::vector<int> adj;
+    int n, m;
+    int* adj;
 };
 
 int main(void)
 {
-	while (!std::cin.eof())
-	{
-		int n, m;
-		std::cin >> n >> m;
+    while (!std::cin.eof())
+    {
+        int n, m;
+        std::cin >> n >> m;
 
-		Graph *g = new Graph(n, m);
+         Graph* g = new Graph(n, m);
 
-		for (int i = 0; i < m; ++i)
-		{
-			int u, v;
-			std::cin >> u >> v;
-			g->set(u - 1, v - 1);
-			g->set(v - 1, u - 1);
-		}
+        for (int i = 0; i < m; i++)
+        {
+            int u, v;
+            std::cin >> u >> v;
+            g->set(u - 1, v - 1);
+            g->set(v - 1, u - 1);
+        }
 
-		int c, r, e;
-		std::cin >> c >> r >> e;
+        int c, r, e;
+        std::cin >> c >> r >> e;
 
-		for (int i = 0; i < n; ++i)
-		{
-			g->unset(i, e - 1);
-			g->unset(e - 1, i);
-		}
+        for (int i = 0; i < n; i++)
+        {
+            g->unset(i, e - 1);
+            g->unset(e - 1, i);
+        }
 
-		std::cout << g->dijkstra(c - 1, r - 1) << std::endl;
+        std::cout << g->dijkstra(c - 1, r - 1) << std::endl;
+        delete g;
+    }
 
-		delete g;
-	}
-
-	return 0;
+    return 0;
 }
 
 Graph::Graph(int n, int m)
 {
-	this->n = n;
-	this->m = m;
+    this->n = n;
+    this->m = m;
 
-	for (int i = 0; i < n * n; ++i)
-		adj.push_back(0);
+    adj = (int*) malloc( n*n * sizeof(int));
+
+    for (int i = 0; i <= n * n; ++i)
+        adj[i] = 0;
+}
+
+Graph::~Graph(){
+    free(adj);
 }
 
 const int Graph::dijkstra(int s, int e) const
 {
-	std::vector<int> costs;
-	std::vector<int> open;
+    std::vector<int> costs;
+    std::vector<int> open;
 
-	for (int i = 0; i < n; ++i)
-	{
-		open.push_back(i);
-		if (i == s) costs.push_back(0);
-		else costs.push_back(INFINITY);
-	}
+    for (int i = 0; i < n; ++i)
+    {
+        open.push_back(i);
+        if (i == s) costs.push_back(0);
+        else costs.push_back(INFINITY);
+    }
 
-	while (!open.empty())
-	{
-		int closest = open[0];
-		for (int o : open)
-			closest = (costs[o] < costs[closest]) ? o : closest;
-		remove(open, closest);
-		if (closest == e) break;
-		for (int o : open)
-		{
-			if (get(closest, o))
-			{
-				int c = get(closest, o) + costs[closest];
-				costs[o] = (costs[o] < c) ? costs[o] : c;
-			}
-		}
-	}
+    while (!open.empty())
+    {
+        int closest = open[0];
+        for (int o : open)
+            closest = (costs[o] < costs[closest]) ? o : closest;
+        remove(open, closest);
+        if (closest == e) break;
+        for (int o : open)
+        {
+            if (get(closest, o))
+            {
+                int c = get(closest, o) + costs[closest];
+                costs[o] = (costs[o] < c) ? costs[o] : c;
+            }
+        }
+    }
 
-	return costs[e];
+    return costs[e];
 }
